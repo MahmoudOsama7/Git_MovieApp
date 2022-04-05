829cf74967bea6687a540e0aee8f62eff9c2226e
mahmoud osama <mahmoud.osaamaa@gmail.com>
2022-04-05 17:52:41 +0200

    update

--- TheMovieApp/app/src/main/java/com/example/myapplication/ui/popular_movie/MovieListActivity.kt
+++ TheMovieApp/app/src/main/java/com/example/myapplication/ui/popular_movie/MovieListActivity.kt
@@ -17,7 +17,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
- * 
+ * osama osama 
  */
 @AndroidEntryPoint
 class MovieListActivity : AppCompatActivity() {
